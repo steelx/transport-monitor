6a8d4c091b351a5db33bb553ade3591f1a2f4c6e
Ajinkya <ajinkyax@pm.me>
2025-11-06 16:59:36 +0530

    feat: integrate Conan 2.x and added Boost dep

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ clang++ --std=c++17 -o test.out test.cpp
 ### London Tube underground layout
 https://ltnm.learncppthroughprojects.com/network-layout.json
 
+### Conan v2 download and install with conanfile.py
+```bash
+conan install . --profile conanprofile.toml --output-folder ./build --build=missing
+
+# CLion "Reload CMake Project" after the above command
+```
+
+cd build
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
+```
+
+
--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,19 @@
-from conans import ConanFile
+from conan import ConanFile
 
 class ConanPackage(ConanFile):
     name = 'network-monitor'
     version = "0.1.0"
 
-    generators = 'cmake_find_package'
+    # Define the settings your project uses
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Use the new Conan 2.x generators
+    generators = "CMakeDeps", "CMakeToolchain"
 
     requires = [
+        "boost/1.89.0"
     ]
+
+    default_options = {
+        "boost/*:shared": False
+    }
